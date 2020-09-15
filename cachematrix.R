e2c98b9e788b88cab468938b1331560b9d092b0b
Yayun-Zhang <71293421+Yayun-Zhang@users.noreply.github.com>
2020-09-15 00:08:30 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-##
 
 ## Write a short comment describing this function: 
 #To creates a special "matrix" object that can cache its inverse 
